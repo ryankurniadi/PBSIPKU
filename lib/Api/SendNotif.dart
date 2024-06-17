ee804977fe187701e86bbd63eab3cab50cfc3950
ryankurniadi <ryankrv@github.com>
2024-06-17 20:50:21 +0700

    Update Kirim Notifikasi

--- lib/Api/SendNotif.dart
+++ lib/Api/SendNotif.dart
@@ -13,33 +13,7 @@ class SendNotif {
 
   sendNotif(String token, String title, String isi) async {
     try {
-      final client = await clientViaServiceAccount(
-          ServiceAccountCredentials.fromJson({
-           
-          }),
-          [firebaseMassageScape]);
-
-      final accesToken = client.credentials.accessToken.data;
-
-      final isiBody = jsonEncode({
-        "message": {
-          "token": "$token",
-          "notification": {"title": "$title", "body": "$isi"},
-          "data": {
-            "value": "msg",
-          }
-        }
-      });
-      var response = await http.post(url,
-          headers: {
-            "Authorization": "Bearer $accesToken",
-            "Content-Type": "application/json"
-          },
-          body: isiBody);
-
-      if (response.statusCode == 200) {
-        print("berhasil dikirim");
-      }
+      
     } catch (e) {
       print(e);
     }
